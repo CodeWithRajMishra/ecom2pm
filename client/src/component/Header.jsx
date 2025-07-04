6ee7aaf2ed5220d2c73cdc9bc976b37ab6ca4193
CodeWithRajMishra <rajmishra3@gmail.com>
2025-07-04 15:00:36 +0530

    project

--- client/src/component/Header.jsx
+++ client/src/component/Header.jsx
@@ -2,10 +2,11 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { FaShoppingCart } from "react-icons/fa";
 const Header=()=>{
     return(
         <>
-               <Navbar bg="primary" data-bs-theme="dark">
+      <Navbar bg="primary" data-bs-theme="dark">
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
@@ -13,9 +14,14 @@ const Header=()=>{
             <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
           </Nav>
+         
+         <span className='itemcount'> 8 </span> 
+
+          <FaShoppingCart className='carticon' /> 
         </Container>
       </Navbar>
-
+   
+    
         </>
     )
 }
